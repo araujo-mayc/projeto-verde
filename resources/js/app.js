911d05925b0491aa801ceec1a1fe9390d7959a12
araujo.mayc <nocyamaraujo@gmail.com>
2024-12-05 15:59:57 -0300

    feat: add prime icon library
    and update global styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+import 'primeicons/primeicons.css'
 import PrimeVue from 'primevue/config';
 import Lara from '@primevue/themes/lara';
 import { definePreset } from '@primevue/themes';
