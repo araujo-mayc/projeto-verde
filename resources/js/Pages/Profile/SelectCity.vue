a36aa00668095285b6b9c5411042f068bd439bbf
araujo.mayc <nocyamaraujo@gmail.com>
2024-12-06 11:29:14 -0300

    feat: add select city page

---
+++ resources/js/Pages/Profile/SelectCity.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="w-full max-w-screen-md min-h-screen flex flex-col py-6 px-4 bg-sky-500">
+      <h1 class="pb-3">Escolha a cidade</h1>
+      <IconField>
+        <InputIcon class="pi pi-search" />
+        <InputText v-model="search" placeholder="Buscar cidade" fluid />
+      </IconField>
+    </div>
+</template>
+
+<script setup>
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
+import { ref } from 'vue'
+
+const search = ref('');
+</script>
+
+<style lang="scss" scoped>
+
+</style>
