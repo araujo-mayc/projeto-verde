a7d5efdd2f353b2783193ad5c4a3b8c7fd6f178a
araujo.mayc <nocyamaraujo@gmail.com>
2024-12-07 13:01:10 -0300

    feat: add snowflake id on user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Godruoyi\Snowflake\Snowflake;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -42,6 +45,10 @@ class User extends Authenticatable
         'updated_at',
     ];
 
+    public function adProduct() {
+        return $this->hasMany(AdProduct::class);
+    }
+
     /**
      * Get the attributes that should be cast.
      *
@@ -54,5 +61,15 @@ class User extends Authenticatable
             'phone_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            $snowflake = app(Snowflake::class);
+            $model->{$model->getKeyName()} = $snowflake->id();
+        });
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'average_rating' => fake()->randomFloat(2, 1, 5),
+            'rating_count' => fake()->randomNumber(2),
         ];
     }
 
