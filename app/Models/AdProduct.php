6032e4f6e94262d12ec4c99b91f71fede041bd86
araujo.mayc <nocyamaraujo@gmail.com>
2024-12-07 13:02:05 -0300

    feat: update adproduct model

--- app/Models/AdProduct.php
+++ app/Models/AdProduct.php
@@ -2,14 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Ramsey\Uuid\Uuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AdProduct extends Model
 {
     /** @use HasFactory<\Database\Factories\AdProductFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'images',
+        'category',
+        'prices',
+        'promotional_price',
+        'quantity',
+        'status',
+        'locate',
+        'tags',
+        'contact_link',
+        'expired_at',
+    ];
+
+    protected $casts = [
+        'prices' => 'array',
+        'tags' => 'array',
+        'promotional_price' => 'float',
+        'quantity' => 'integer',
+        'images' => 'array',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
         /**
      * Get the attributes that should be cast.
@@ -21,5 +55,14 @@ class AdProduct extends Model
         return [
             'expired_at' => 'datetime',
         ];
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            $model->id = Uuid::uuid4()->toString();
+        });
     }
 }
