5037ee49b6aff5caf432cd73058cd3c0fe05985a
araujo.mayc <nocyamaraujo@gmail.com>
2024-12-17 12:51:36 -0300

    feat: add responsive menu options

---
+++ resources/js/Components/Menu.vue
@@ -0,0 +1,63 @@
+<template>
+      <div v-if="!hideMenu" class="flex justify-center gap-x-5 ">
+        <Button 
+        :severity="activedPage === 'home' ? 'primary' : 'secondary'" @click="redirectTo('/cidades')"
+        size="small" class="!py-1" text>
+          <div class="flex flex-col">
+            <span class="material-icons">dashboard</span>
+            <span class="font-base text-xs">Anúncios</span>
+          </div>
+        </Button>
+        <Button 
+        :severity="activedPage === 'my_ads' ? 'primary' : 'secondary'" @click="redirectTo('/perfil/meus-anuncios')"
+        size="small" class="!py-1" text>
+          <div class="flex flex-col">
+            <span class="material-icons">folder</span>
+            <span class="font-base text-xs">Meus Anúncios</span>
+          </div>
+        </Button>
+        <Button 
+          :severity="activedPage === 'notifications' ? 'primary' : 'secondary'" @click="redirectTo('notifications')" 
+          size="small" class="!py-1" text >
+          <div class="flex flex-col">
+            <span class="material-icons">notifications</span>
+            <span class="font-base text-xs">Notificações</span>
+          </div>
+        </Button>
+        <Button 
+          :severity="activedPage === 'profile' ? 'primary' : 'secondary'" @click="redirectTo('profile')"
+          size="small" class="!py-1" text>
+          <div class="flex flex-col">
+            <span class="material-icons">account_circle</span>
+            <span class="font-base text-xs">Perfil</span>
+          </div>
+        </Button>
+      </div>
+</template>
+
+<script setup>
+import { router } from '@inertiajs/vue3';
+import Button from 'primevue/button';
+
+defineProps({
+  hideMenu: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  activedPage: {
+    type: String,
+    required: false,
+    default: 'home'
+  }
+});
+
+const redirectTo = (route) => {
+  if (route) {
+    router.visit(route, {
+      method: 'get',
+    });
+  }
+};
+
+</script>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,45 +1,15 @@
 <template>
   <div class="w-full min-h-screen flex flex-col justify-between bg-[var(--neutral-200)]">
-  <header class="flex justify-between py-2 px-6 bg-white shadow-md">
-    <div class="flex items-center">
-        <img class="size-12 mx-2 rounded-full" src="https://placehold.co/600?text=Logo" alt="" />
-        <h2 class="font-bold text-sky-500">Feirine</h2>
-    </div>
-    <div v-if="!hideMenu" class="flex justify-center gap-x-5 ">
-      <Button 
-      :severity="activedPage === 'home' ? 'primary' : 'secondary'" @click="redirectTo('/cidades')"
-      size="small" class="!p-1" text>
-        <div class="flex flex-col">
-          <span class="material-icons">dashboard</span>
-          <span class="font-base text-xs">Anúncios</span>
-        </div>
-      </Button>
-      <Button 
-      :severity="activedPage === 'my_ads' ? 'primary' : 'secondary'" @click="redirectTo('/perfil/meus-anuncios')"
-      size="small" class="!p-1" text>
-        <div class="flex flex-col">
-          <span class="material-icons">folder</span>
-          <span class="font-base text-xs">Meus Anúncios</span>
-        </div>
-      </Button>
-      <Button 
-        :severity="activedPage === 'notifications' ? 'primary' : 'secondary'" @click="redirectTo('notifications')" 
-        size="small" class="!p-1" text >
-        <div class="flex flex-col">
-          <span class="material-icons">notifications</span>
-          <span class="font-base text-xs">Notificações</span>
-        </div>
-      </Button>
-      <Button 
-        :severity="activedPage === 'profile' ? 'primary' : 'secondary'" @click="redirectTo('profile')"
-        size="small" class="!p-1" text>
-        <div class="flex flex-col">
-          <span class="material-icons">account_circle</span>
-          <span class="font-base text-xs">Perfil</span>
-        </div>
-      </Button>
-    </div>
-  </header>
+    <header v-if="!ismobile" class="flex justify-between py-2 px-6 bg-white shadow-md">
+      <div class="flex items-center">
+          <img class="size-12 mx-2 rounded-full" src="https://placehold.co/600?text=Logo" alt="" />
+          <h2 class="font-bold text-sky-500">Feirine</h2>
+      </div>
+      <Menu />
+    </header>
+    <section v-else name="menu" class="w-full px-2 py-1 fixed bottom-0 bg-gray-100" >
+      <Menu />
+    </section>
   <main class="flex grow justify-center bg-[var(--neutral-200)] mt-2">
       <slot name="main" />
   </main>
@@ -58,32 +28,19 @@
 
 <script setup>
 import { ref } from 'vue';
-import Button from 'primevue/button';
+import Menu from '@/Components/Menu.vue';
 import Divider from 'primevue/divider';
-import { router } from '@inertiajs/vue3';
+const ismobile = ref(false);
 
-defineProps({
-  hideMenu: {
-    type: Boolean,
-    required: false,
-    default: false
-  },
-  activedPage: {
-    type: String,
-    required: false,
-    default: 'home'
-  }
-});
-
-
-
-
-
-const redirectTo = (route) => {
-  if (route) {
-    router.visit(route, {
-      method: 'get',
-    });
+const handleResize = () => {
+  if (window.innerWidth <= 640) {
+    ismobile.value = true;
+  } else {
+    ismobile.value = false;
   }
 };
+
+window.addEventListener('resize', handleResize);
+handleResize();
 </script>
+
